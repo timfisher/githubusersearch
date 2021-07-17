a284f1437a4f845ec298400123f29a7848771f5f
Timothy Fisher <tim.fisher@tim.fisher>
2021-07-17 14:14:12 +0100

    test: more tests

--- cypress/integration/publicRepositorySearch.feature
+++ cypress/integration/publicRepositorySearch.feature
@@ -5,6 +5,7 @@ Feature: Public Repository Search
     Background: User is on the search page
         Given the user is on the search page
 
+    @smoke
     Scenario Outline: Searching for a user that exists shows their public repositories
         Given the user searches for <username>
         Then the <repository> is shown
@@ -13,3 +14,14 @@ Feature: Public Repository Search
             | username  | repository |
             | timfisher | ddd        | 
             | timfisher | bitmon     |
+    
+    Scenario Outline: Searching for a user that exists shows their public repositories
+        Given there are mock user results
+        Given the user searches for <username>
+        Then the <repository> is shown
+        Then the <repository> has <stars> stars
+        Then the <repository> has <watchers> watchers
+
+        Examples:
+            | username  | repository           | stars | watchers |
+            | timfisher | topQualityRepository | 9999  | 9999     |
--- cypress/integration/publicRepositorySearch/publicRepositorySearch.ts
+++ cypress/integration/publicRepositorySearch/publicRepositorySearch.ts
@@ -1,7 +1,46 @@
-import { Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then } from "cypress-cucumber-preprocessor/steps";
+import { SearchUserQuery } from "../../../src/queries";
 import { anyCaseRegex } from "../common/helpers";
 
 Then(`the {} is shown`, (respositoryName: string) => {
   const repositoryAriaLabel = `${respositoryName} repository`;
   cy.findByRole("listitem", { name: anyCaseRegex(repositoryAriaLabel) });
+});
+
+Given(`there are mock user results`, () => {
+  cy.fixture("mockUserResults.json")
+    .then((userResults: SearchUserQuery) => {
+      cy.intercept("https://api.github.com/graphql", userResults);
+    })
+    .as("userResults");
+});
+
+Then(
+  `the {} has {int} stars`,
+  (respositoryName: string, numberOfStars: number) => {
+    const repositoryAriaLabel = `${respositoryName} repository`;
+    const starsAriaLabel = `${numberOfStars} users starred this repository`;
+    cy.findByRole("listitem", {
+      name: anyCaseRegex(repositoryAriaLabel),
+    }).findByRole("generic", { name: starsAriaLabel });
+  }
+);
+
+Then(
+  `the {} has {int} watchers`,
+  (respositoryName: string, numberOfWatchers: number) => {
+    const repositoryAriaLabel = `${respositoryName} repository`;
+    const watchersAriaLabel = `${numberOfWatchers} users watched this repository`;
+    cy.findByRole("listitem", {
+      name: anyCaseRegex(repositoryAriaLabel),
+    }).findByRole("generic", { name: watchersAriaLabel });
+  }
+);
+
+Then(`the repository {} has {int} watchers`, () => {
+  cy.fixture("mockUserResults.json")
+    .then((userResults: SearchUserQuery) => {
+      cy.intercept("https://api.github.com/graphql", userResults);
+    })
+    .as("userResults");
 });
